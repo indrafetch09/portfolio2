104f3daf428ab8ae02e7d1d49d8f5ce9f4870d93
Kaiden <harindrawahyu131@gmail.com>
2026-02-19 15:08:39 +0700

    change color blue, change all correct css variable, add main to terminal panel

--- src/components/HeadCmd.jsx
+++ src/components/HeadCmd.jsx
@@ -1,4 +1,4 @@
-import { IoGitBranchOutline } from "react-icons/io5";
+import { IoGitBranch } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa";
 
 const HeadCmd = () => {
@@ -6,23 +6,23 @@ const HeadCmd = () => {
         <>
             {/* head */}
             <div class="flex items-center gap-4">
-                <span class="font-bold text-lg text-(--text-accent-tosca)">~</span>
+                <span class="font-bold text-lg text-accent-tosca">~</span>
                 {/* horizontal line */}
-                <div class="grow border-t border-(--text-muted)"></div>
-                <div className="flex gap-2 text-sm font-bold text-(--text-main)">
-                    <p className="font-normal">on</p>
-                    <div class="flex gap-1 accent-red">
-                        <IoGitBranchOutline />
-                        <span > main</span>
+                <div class="grow border-t border-main"></div>
+                <div className="flex gap-2 text-sm font-bold text-main">
+                    <span className="font-normal">on</span>
+                    <div class="flex gap-1 text-accent-purple">
+                        <IoGitBranch />
+                        <span >main</span>
                     </div>
-                    <p className="font-normal">via</p>
-                    <div class="flex gap-1 ubderline accent-green">
+                    <span className="font-normal">via</span>
+                    <div class="flex gap-1 ubderline text-accent-green">
                         <FaGithub />
                         <a className="underline" href="https://github.com/indrafetch09">
                             indrafetch09
                         </a>
                     </div>
-                    <p className="text-(--text-accent-purple)">&copy;-2026-Copyright</p>
+                    <p>&copy; Copyright-2026</p>
                 </div>
             </div>
             <div class="flex text-sm gap-2">
